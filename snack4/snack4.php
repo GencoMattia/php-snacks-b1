b4f4ea49fe64cf4faa2467bc8e59ed2fc19ee146
GencoMattia <gencomattia@hotmail.it>
2024-06-26 16:09:00 +0200

    corrected an error in the if logic

--- snack4/snack4.php
+++ snack4/snack4.php
@@ -9,7 +9,11 @@ $randomNumberList = [
 
 while (count($randomNumberList) < 15) {
     $randomNumber = random_int(1, 50);
-    if(array_search($randomNumber, $randomNumberList) === true) {
+    if(array_search($randomNumber, $randomNumberList) === false) {
         $randomNumberList[] = $randomNumber;
     }
 };
+
+var_dump($randomNumberList);
+
+?>
