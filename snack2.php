560702af25af959270c2c07bd4ce5813c3c61934
GencoMattia <gencomattia@hotmail.it>
2024-06-26 12:47:30 +0200

    added get for userName, email, userAge

---
+++ snack2.php
@@ -0,0 +1,10 @@
+<!-- Snack 2
+Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
+
+<?php
+
+$userName = $_GET["userName"];
+$email = $_GET["email"];
+$userAge = $_GET["userAge"];
+
+?>
