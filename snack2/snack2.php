1598fb6822a8d97ffd9825ef0493c1c8844e155b
GencoMattia <gencomattia@hotmail.it>
2024-06-26 15:10:55 +0200

    corrected a condition in the inside the if declaration. Updated html structure

--- snack2/snack2.php
+++ snack2/snack2.php
@@ -7,7 +7,7 @@ $userName = $_GET["userName"] ?? "";
 $email = $_GET["email"] ?? "";
 $userAge = $_GET["userAge"] ?? "";
 
-if(strlen($userName) <= 3 && filter_var($email, FILTER_VALIDATE_EMAIL) && filter_var($userAge, FILTER_VALIDATE_INT)) {
+if(strlen($userName) >= 3 && filter_var($email, FILTER_VALIDATE_EMAIL) && filter_var($userAge, FILTER_VALIDATE_INT)) {
     echo "Access Granted";
 } else {
     echo "Access Denied!";
@@ -16,18 +16,27 @@ if(strlen($userName) <= 3 && filter_var($email, FILTER_VALIDATE_EMAIL) && filter
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Accesso</title>
 </head>
 <body>
-    <form action="./snack2.php" method="GET">
-        <input type="text" name="userName" id="userName">
-        <input type="email" name="email" id="email">
-        <input type="number" name="userAge" id="userAge">
-        <button>Submit</button>
+    <form action="snack2.php" method="GET">
+        <div>
+            <label for="userName">Nome utente:</label>
+            <input type="text" name="userName" id="userName" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+        <div>
+            <label for="userAge">Età:</label>
+            <input type="number" name="userAge" id="userAge" required>
+        </div>
+        <button type="submit">Invia</button>
     </form>
 </body>
 </html>
