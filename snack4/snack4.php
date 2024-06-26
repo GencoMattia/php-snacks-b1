cf430c1920d75105c77d663892030aea69b85b17
GencoMattia <gencomattia@hotmail.it>
2024-06-26 16:05:02 +0200

    first attempt at populating the randomNumberList array

--- snack4/snack4.php
+++ snack4/snack4.php
@@ -1,2 +1,15 @@
 <!-- Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+
+<?php
+$randomNumberList = [
+
+];
+
+while (count($randomNumberList) < 15) {
+    $randomNumber = random_int(1, 50);
+    if(array_search($randomNumber, $randomNumberList) === true) {
+        $randomNumberList[] = $randomNumber;
+    }
+};
