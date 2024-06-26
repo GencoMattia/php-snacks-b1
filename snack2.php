f0817d2367b5fb9115ba01f2d1c11ccdf362e8da
GencoMattia <gencomattia@hotmail.it>
2024-06-26 12:55:37 +0200

    added if statement

--- snack2.php
+++ snack2.php
@@ -7,4 +7,10 @@ $userName = $_GET["userName"];
 $email = $_GET["email"];
 $userAge = $_GET["userAge"];
 
+if(strlen($userName) <= 3 && filter_var($email, FILTER_VALIDATE_EMAIL) && filter_var($userAge, FILTER_VALIDATE_INT)) {
+    echo "Access Granted";
+} else {
+    
+}
+
 ?>
