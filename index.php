05df45bd162e3b30e92b51f769bdca0a27966344
GencoMattia <gencomattia@hotmail.it>
2024-06-26 12:22:44 +0200

    added first match inside the array

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <!-- Snack 1
 Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+
+$matchList = [
+    [
+        "Squadra di casa" => "Draghi Rossi",
+        "Squadra Ospite" => "Aquile Bianche",
+        "Stadio" => "Palazzetto dello Sport -Fuoco e Ghiaccio-",
+        "Risultato" => rand(1,100) . " | " . rand(1,100),
+    ],
+]
+
+?>
