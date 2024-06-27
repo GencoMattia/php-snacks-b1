9b2f7bf5e1197611a0328fbacdcb605961d7b16f
GencoMattia <gencomattia@hotmail.it>
2024-06-27 12:25:17 +0200

    included array.php in snack7.php

--- snack7/snack7.php
+++ snack7/snack7.php
@@ -2,3 +2,8 @@
 Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
 Questo array dovra' essere inserito in un file separato, da importare.
 Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+<?php 
+include __DIR__ . "/utilities/array.php";
+
+?>
