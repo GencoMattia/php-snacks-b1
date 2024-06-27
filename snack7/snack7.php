8a57cdcd058927d428279f1478745dfeb8b496e9
GencoMattia <gencomattia@hotmail.it>
2024-06-27 12:22:09 +0200

    added snack 7 folders with php files and snack description

---
+++ snack7/snack7.php
@@ -0,0 +1,4 @@
+<!-- Snack 7
+Creare un array contenente qualche alunno di un'ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+Questo array dovra' essere inserito in un file separato, da importare.
+Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
