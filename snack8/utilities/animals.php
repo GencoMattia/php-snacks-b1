ac0e86ffc621f31e0a7731ab2e696d0448ad14e1
GencoMattia <gencomattia@hotmail.it>
2024-06-27 12:48:07 +0200

    added animals array

---
+++ snack8/utilities/animals.php
@@ -0,0 +1,13 @@
+<?php
+$animals = [
+    ['nome' => 'Leone', 'specie' => 'Panthera leo', 'classe' => 'Mammifero'],
+    ['nome' => 'Elefante africano', 'specie' => 'Loxodonta africana', 'classe' => 'Mammifero'],
+    ['nome' => 'Gatto', 'specie' => 'Felis catus', 'classe' => 'Mammifero'],
+    ['nome' => 'Squalo bianco', 'specie' => 'Carcharodon carcharias', 'classe' => 'Pesce'],
+    ['nome' => 'Tonno rosso', 'specie' => 'Thunnus thynnus', 'classe' => 'Pesce'],
+    ['nome' => 'Salmone', 'specie' => 'Salmo salar', 'classe' => 'Pesce'],
+    ['nome' => 'Cobra reale', 'specie' => 'Ophiophagus hannah', 'classe' => 'Rettile'],
+    ['nome' => 'Tartaruga marina', 'specie' => 'Caretta caretta', 'classe' => 'Rettile'],
+    ['nome' => 'Coccodrillo del Nilo', 'specie' => 'Crocodylus niloticus', 'classe' => 'Rettile'],
+    ['nome' => 'Aquila reale', 'specie' => 'Aquila chrysaetos', 'classe' => 'Uccello'] 
+];
