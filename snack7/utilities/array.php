f2c6aacbe0cd06ea475c4603eb52dd9cc70d58d9
GencoMattia <gencomattia@hotmail.it>
2024-06-27 12:23:48 +0200

    added array in array.php

---
+++ snack7/utilities/array.php
@@ -0,0 +1,18 @@
+<?php
+$classe = [
+    [
+        'Nome' => 'Mario',
+        'Cognome' => 'Rossi',
+        'Voti' => [8, 6, 7, 9]
+    ],
+    [
+        'Nome' => 'Giulia',
+        'Cognome' => 'Verdi',
+        'Voti' => [7, 5, 8, 8]
+    ],
+    [
+        'Nome' => 'Luca',
+        'Cognome' => 'Bianchi',
+        'Voti' => [9, 9, 7, 6]
+    ]
+];
