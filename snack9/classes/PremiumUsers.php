79ede70658247f3e1fae74e3ec2d83a157f873a8
GencoMattia <gencomattia@hotmail.it>
2024-07-03 12:34:23 +0200

    hopefully composite membership into premiumUsers

--- snack9/classes/PremiumUsers.php
+++ snack9/classes/PremiumUsers.php
@@ -1,16 +1,19 @@
 <?php 
 
 require_once __DIR__ . "/Users.php";
+require_once __DIR__ . "/Membership.php";
 
 /**
  * Create a PremiumUser class obj that extend User class
  */
 class PremiumUser extends User {
     private $password;
+    private $benefits;
 
-    public function __construct($username, $password){
+    public function __construct($username, $password, $benefits){
         parent::__construct($username);
         $this -> password = $password;
+        $this -> benefits = $benefits;
     }
 
     /**
@@ -30,6 +33,10 @@ class PremiumUser extends User {
      */
     public function setPassword($password){
         return $this -> password = $password;
+    }
+
+    public function getBenefits(){
+        return $this -> benefits;
     }
 }
 
