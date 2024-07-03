77cfa4c00db0a399ce072197eae9c8607916c367
GencoMattia <gencomattia@hotmail.it>
2024-07-03 12:14:22 +0200

    created username class

--- snack9/classes/Users.php
+++ snack9/classes/Users.php
@@ -1,5 +1,33 @@
 <?php 
 
+/**
+ * Create a User calls obj
+ */
+class User {
+    private $username;
 
+    public function __construct($username){
+        $this -> username = $username;
+    }
+
+    /**
+     * get the class username istance
+     *
+     * @return void
+     */
+    public function getUsername(){
+        return $this -> username;
+    }
+
+    /**
+     * Set the call istance username
+     *
+     * @param [string] $username
+     * @return void
+     */
+    public function setUsername($username){
+        return $this -> username = $username;
+    }
+}
 
 ?>
