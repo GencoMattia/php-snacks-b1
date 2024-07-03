c166475fddfd9f0b3247cce10a9b02edb6b528c0
GencoMattia <gencomattia@hotmail.it>
2024-07-03 12:23:04 +0200

    created PremiumUser class

--- snack9/classes/PremiumUsers.php
+++ snack9/classes/PremiumUsers.php
@@ -1,5 +1,36 @@
 <?php 
 
+require_once __DIR__ . "/Users.php";
 
+/**
+ * Create a PremiumUser class obj that extend User class
+ */
+class PremiumUser extends User {
+    private $password;
+
+    public function __construct($username, $password){
+        parent::__construct($username);
+        $this -> password = $password;
+    }
+
+    /**
+     * Return class istance password
+     *
+     * @return void
+     */
+    public function getPassword(){
+        return $this -> password;
+    }
+
+    /**
+     * set class istance password as a string
+     *
+     * @param [string] $password
+     * @return void
+     */
+    public function setPassword($password){
+        return $this -> password = $password;
+    }
+}
 
 ?>
