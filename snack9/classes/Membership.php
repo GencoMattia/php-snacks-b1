a42fe425e1c01f3ce13cdff13b893d526a2e36bc
GencoMattia <gencomattia@hotmail.it>
2024-07-03 12:27:56 +0200

    created membership class

--- snack9/classes/Membership.php
+++ snack9/classes/Membership.php
@@ -1,5 +1,33 @@
 <?php 
 
+/**
+ * create a Membership class obj
+ */
+class Membership {
+    private $benefits;
 
+    public function __construct($benefits){
+        $this -> benefits = $benefits;
+    }
+
+    /**
+     * return benefits class instance
+     *
+     * @return void
+     */
+    public function getBenefits(){
+        return $this -> benefits;
+    }
+
+    /**
+     * set benefits class instance as a string
+     *
+     * @param [string] $benefits
+     * @return void
+     */
+    public function setBenefits($benefits){
+        return $this -> benefits = $benefits;
+    }
+}
 
 ?>
